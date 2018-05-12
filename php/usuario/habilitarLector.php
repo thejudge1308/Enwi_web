f8331181f38bb83e29da63787fafced524e6bd4a
Gerardo Patricio <gestrada12@alumnos.utalca.cl>
2018-05-12 17:38:27 -0300

    Creacion de habilitar lector y cambios.
    Creacion de habilitarLector.php y modificacion en deshabilitarLector.php

--- php/usuario/habilitarLector.php
+++ php/usuario/habilitarLector.php
@@ -1,6 +1,7 @@
 <?php 
 	include "../db.php";
 	header("Content-Type: application/json; charset=UTF-8");
+	
 	$rut = $_POST["rut"];
 
 	$sql = "SELECT * FROM lector WHERE rut=".$rut;
@@ -11,18 +12,18 @@
 		Pregunta si el usuario existe
 	*/
 	if ($resultado->num_rows > 0) {
-		$sql = "SELECT * FROM lector WHERE rut=".$rut." AND estado = false";
+		$sql = "SELECT * FROM lector WHERE rut=".$rut." AND estado = true";
 		$resultado = mysqli_query($conn, $sql);
 		/**
 			Pregunta si el usuario ya esta deshabilitado
 		*/
 		if($resultado->num_rows > 0){
 			$foo->mensaje = false;
-			$foo->mensaje = "El lector ya esta deshabilitado";
+			$foo->mensaje = "El lector ya esta habilitado";
 			echo json_encode($foo);
 			
 		}else{
-			$sql = "UPDATE lector SET estado = \"false\" WHERE rut = ".$rut;
+			$sql = "UPDATE lector SET estado = \"true\" WHERE rut = ".$rut;
 			$resultado = mysqli_query($conn, $sql);
 			$foo->mensaje = true;
 			$foo->mensaje = "Se ha modificado exitosamente";
