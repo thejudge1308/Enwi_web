0605c04c994ddc4b2ae9146a0269f6f2b921ee8b
Unknown <marcelmarch95@gmail.com>
2018-05-15 02:22:31 -0400

    Auto stash before merge of "master" and "origin/master"
    agregados: insert libros and insert lectores

--- php/usuario/obtenerLectores.php
+++ php/usuario/obtenerLectores.php
@@ -1,4 +1,5 @@
 <?php 
+<<<<<<< Updated upstream
 	include "../db.php";
 	header("Content-Type: application/json; charset=UTF-8");
 
@@ -26,6 +27,32 @@
 		$foo->mensaje = false;
 		echo json_encode($foo);
 	}
+=======
+include "../db.php";
+//header("Content-Type: application/json; charset=UTF-8");
+
+$sql = "SELECT * FROM lector";
+$result = mysqli_query($conn, $sql);
+$foo = new StdClass();
+
+
+if ($result->num_rows > 0) 
+{
+    //echo "true"; 
+	$foo = true;
+	$rows = array();
+		while($r = mysqli_fetch_assoc($result)) {
+			
+		    $rows[] = $r;
+		}
+		echo json_encode($rows);
+}
+else 
+{
+	//echo "false";
+	echo json_encode(null);
+}
+>>>>>>> Stashed changes
 
 	mysqli_close($conn);
 ?>
