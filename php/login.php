a15b3fc3088831f6088e973a976d2d631fcdece7
Gerardo Patricio <gestrada12@alumnos.utalca.cl>
2018-05-17 01:23:24 -0400

    Modificacion login php, con perfiles de usuario

--- php/login.php
+++ php/login.php
@@ -4,19 +4,25 @@ include "db.php";
 $usuario = $_POST["correo"];
 $contrasena = $_POST["contasena"];
 
-$sql = "SELECT * FROM trabajador WHERE correoElectronico = '".$usuario."' AND contrasena ='".$contrasena."'";
+$sql = "SELECT tipo FROM trabajador WHERE correoElectronico = '".$usuario."' AND contrasena = '".$contrasena."';";
+$foo = new StdClass();
 
-//$sql="SELECT * FROM trabajador WHERE correoElectronico = 'pato@pato.com' AND contrasena ='1234'";
+//$sql="SELECT tipo FROM trabajador WHERE correoElectronico = 'pato@pato.com'AND contrasena ='1234';";
 $result = mysqli_query($conn, $sql);
-//echo mysqli_num_rows($result);
 
-if ($result->num_rows > 0) {
 
-   echo "true";
+if (mysqli_num_rows($result) > 0) {
+	$row = mysqli_fetch_array($result);
+	//print_r($row);
+	$foo->mensaje = "true";
+	$foo->tipo = $row[0];
+	echo json_encode($foo);
 } else {
-   echo "falso";
+	$foo->mensaje = "false";
+	echo json_encode($foo);
 }
 
+mysqli_free_result($result);
 mysqli_close($conn);
 
 
