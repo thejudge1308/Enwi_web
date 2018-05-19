1627dc87c09c530059c1adce1d5b853a17d04c25
Patricio Quezada <the.judge16@gmail.com>
2018-05-19 14:46:49 -0400

    Update obtenerLectores.php

--- php/usuario/obtenerLectores.php
+++ php/usuario/obtenerLectores.php
@@ -1,39 +1,10 @@
 <?php 
-<<<<<<< Updated upstream
-=======
-	include "../db.php";
-	header("Content-Type: application/json; charset=UTF-8");
->>>>>>> Stashed changes
-
-
-<<<<<<< Updated upstream
 include "../db.php";
-//header("Content-Type: application/json; charset=UTF-8");
+header("Content-Type: application/json; charset=UTF-8");
 
 $sql = "SELECT * FROM lector";
 $result = mysqli_query($conn, $sql);
 $foo = new StdClass();
-
-
-if ($result->num_rows > 0) 
-{
-    //echo "true"; 
-	$foo = true;
-	$rows = array();
-		while($r = mysqli_fetch_assoc($result)) {
-			
-		    $rows[] = $r;
-		}
-		echo json_encode($rows);
-}
-else 
-{
-	//echo "false";
-	echo json_encode(null);
-}
-
-=======
-	$foo = new StdClass();
 
 	if ($result->num_rows > 0) 
 	{
@@ -54,7 +25,6 @@ else
 		$foo->mensaje = "false";
 		echo json_encode($foo);
 	}
->>>>>>> Stashed changes
 
 	mysqli_close($conn);
 ?>
