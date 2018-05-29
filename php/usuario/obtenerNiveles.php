73547d405b76d93dfa46314fb451e888a58a7216
Unknown <jnfco_18@hotmail.com>
2018-05-29 01:04:43 -0400

    Vincular la asociacion de copias a niveles y estantes en Base de datos

---
+++ php/usuario/obtenerNiveles.php
@@ -0,0 +1,32 @@
+<?php 
+include "../db.php";
+header("Content-Type: application/json; charset=UTF-8");
+
+$codigoEstante = $_POST["codigoEstante"];
+
+$sql = "SELECT * FROM nivel WHERE codigoEstante = '".$codigoEstante."'";
+$result = mysqli_query($conn, $sql);
+$foo = new StdClass();
+
+	if ($result->num_rows > 0) 
+	{
+	   //echo "true"; ->mensaje tipo json
+		$foo->mensaje = "true";
+		$rows = array();
+		while($r = mysqli_fetch_assoc($result)) {
+			$rows[] = $r;
+		}
+		
+			//$foo=$rows;
+		$foo->datos = $rows;
+		echo json_encode($foo);
+	}
+	//else : enviar mensaej de q se ingreso 
+	else 
+	{
+		$foo->mensaje = "false";
+		echo json_encode($foo);
+	}
+
+	mysqli_close($conn);
+?>
